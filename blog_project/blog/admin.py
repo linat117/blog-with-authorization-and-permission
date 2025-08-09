c561cc162d0b524b0db0a84ebf8bf9f16be67f19
Hilina Teshome <teshilina8@gmail.com>
2025-08-09 20:44:29 +0300

    set up admin for post model

--- blog_project/blog/admin.py
+++ blog_project/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published', 'created_at')
+    search_fields = ('title', 'author', 'published', 'created_at')
 
-# Register your models here.
+    
